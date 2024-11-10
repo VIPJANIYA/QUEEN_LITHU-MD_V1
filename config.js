35ca499038ab6293784997454e84bb4ec68e5b4e
LithumHelitha <155241749+LithumHelitha@users.noreply.github.com>
2024-11-10 08:57:49 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+SESSION_ID: process.env.SESSION_ID || "Enter Your Session Code",
+MONGODB: process.env.MONGODB || "mongodb+srv://lithumhelitha2008:7rDwXUVeTnIfmnjl@cluster0.1d0bq.mongodb.net/",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://unitedcamps.in/Images/file_5278.jpg",
+AUTO_READ_CMD: process.env.AUTO_READ_CMD || "true",
+AUTO_BIO:"true"
+};
