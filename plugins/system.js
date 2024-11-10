4e0746ffd3b7184a3fddb11dd180c78a89442cb7
LithumHelitha <155241749+LithumHelitha@users.noreply.github.com>
2024-11-10 17:17:34 +0530

    Create system.js

---
+++ plugins/system.js
@@ -0,0 +1,28 @@
+const {cmd , commands} = require('../command')
+const os = require("os")
+const {runtime} = require('../lib/functions')
+
+cmd({
+    pattern: "system",
+    alias: ["status","botinfo"],
+    desc: "check up time",
+    category: "main",
+    react: "🧬",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{ 
+
+let status =`
+┌───────────────────◉▷
+┝ ✨ *Runtime:-  ${runtime(process.uptime())}*    
+┝ 🎁 *Ram usage:- ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB*
+┝ 🦕 *Platform:- ${os.hostname()}*
+┝ 🥀 *Owner:- 𝙳𝙰𝚁𝙺 𝚃𝙴𝙲𝙷-𝚇*   
+└───────────────────◉▷`
+return reply(`${status}`)
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
