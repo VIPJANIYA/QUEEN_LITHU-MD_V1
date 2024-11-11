943a7f58f32856a5ac72cca19ff8b7bb64339e35
LithumHelitha <155241749+LithumHelitha@users.noreply.github.com>
2024-11-11 18:56:23 +0530

    Update hack.js

--- plugins/hack.js
+++ plugins/hack.js
@@ -36,7 +36,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
             '⚠️ *Note:* All actions are for demonstration purposes only.',
             '⚠️ *Reminder:* Ethical hacking is the only way to ensure security.',
             '',
-            '> *SAHAS-MD-HACKING-COMPLETE ☣*'
+            '> *QUEEN-LITHU-MD-HACKING-COMPLETE ☣*'
         ];
 
         for (const line of steps) {
