0809e239f76c7f2b1516a7314e1eed12616be4bd
LithumHelitha <155241749+LithumHelitha@users.noreply.github.com>
2024-11-14 18:42:13 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "ONIThITB#HPRl8H-755ruK_RhvONWL7wL4WFUM-_8tN28h8Uu8fo",
+SESSION_ID: process.env.SESSION_ID || "please enter your session code 😊😊",
 MONGODB: process.env.MONGODB || "mongodb+srv://lithumhelitha2008:7rDwXUVeTnIfmnjl@cluster0.1d0bq.mongodb.net/",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://unitedcamps.in/Images/file_5278.jpg",
 AUTO_READ_CMD: process.env.AUTO_READ_CMD || "true",
