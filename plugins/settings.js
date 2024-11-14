e7e3f7cb1138350e78e0902e4009452cf0a8e92d
LithumHelitha <155241749+LithumHelitha@users.noreply.github.com>
2024-11-14 19:41:21 +0530

    Update settings.js

--- plugins/settings.js
+++ plugins/settings.js
@@ -63,7 +63,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
 
 > *©ᴘᴏᴡᴇʀᴇᴅ Qᴜᴇᴇɴ-ʟɪᴛʜᴜ-ᴍᴅ*`;
 
-        const vv = await conn.sendMessage(from, { image: { url: "https://files.catbox.moe/de82e3.jpg"}, caption: desc }, { quoted: mek });
+        const vv = await conn.sendMessage(from, { image: { url: "https://ibb.co/F0ccn0P"}, caption: desc }, { quoted: mek });
 
         conn.ev.on('messages.upsert', async (msgUpdate) => {
             const msg = msgUpdate.messages[0];
