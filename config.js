5fc9edcaaa8b90cdfa4d059c2bded55c74d1e22c
VIPJANIYA <rmkarunarathna052@gmail.com>
2024-11-20 00:34:47 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",     // මෙතනට ඔයාගෙ සෙශන් අයි ඩී එක දාන්න
+SESSION_ID: process.env.SESSION_ID || "PENhACwB#4fg6Udb14mHYruTuRgN91AgD9oiHQJl_oVh2iQ6mhJU",     // මෙතනට ඔයාගෙ සෙශන් අයි ඩී එක දාන්න
 MONGODB: process.env.MONGODB || "mongodb+srv://lithumhelitha2008:7rDwXUVeTnIfmnjl@cluster0.1d0bq.mongodb.net/",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/myW7D5f/2df27fbd1a812920.jpg",
 AUTO_READ_CMD: process.env.AUTO_READ_CMD || "true",
